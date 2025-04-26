6ac958bb15f92e01dec57ccce6c2a279ad4d6c49
Vislavath Gopal <vislavathgopal644@gmail.com>
2025-04-26 15:22:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = 'mongodb+srv://vislavathgopal644:cjoNq3gmMLGkKpC4@cluster1.8eukton.mongodb.net/student_management?retryWrites=true&w=majority';
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Routes
+
 app.use('/students', studentRoutes);
 
-// Connect to MongoDB and start server
+app.get('/', (req, res) => {
+  res.send('Welcome to the Student Management API!');
+});
+
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
